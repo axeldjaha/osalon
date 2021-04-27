d8ec63839ebafa277fd0bedb798ab75782f28544
DR DJAHA <paxeldp@gmail.com>
2021-04-27 17:19:42 +0000

    Update

--- app/Http/Controllers/Api/AbonnementController.php
+++ app/Http/Controllers/Api/AbonnementController.php
@@ -8,6 +8,7 @@ class AbonnementController extends ApiController
 {
     public function index()
     {
+
         $abonnement = $this->compte->abonnements()->orderBy("id", "desc")->first();
         return response()->json(new AbonnementResource($abonnement));
     }
